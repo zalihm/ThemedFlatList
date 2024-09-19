2c198e9ac94fd15092513fd9397f15067c8f09e9
mzalih <salih@toobler.com>
2024-09-19 15:40:31 +0530

    Initial commit
    Generated by react-native@0.75.3

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
 export { default as ThemedFlatList } from './src/Comp/ThemedFlatList';
-=======
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
->>>>>>> cef98fa (updated)
